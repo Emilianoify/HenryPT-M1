e7484b45e00600bf275c1a1161c93a6d166101fa
unknown <galvanalvarezemiliano@hotmail.com>
2022-08-03 19:36:48 -0300

    Homework Finished

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,34 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let dec = 0
+  let suma;
+  let str
+  str = num.toString()
+  let binarios = str.split('')
+  let binariosR = binarios.reverse()
+  for(let i = 0; i < binarios.length; i++){
+    suma = binariosR[i] * Math.pow(2, i)
+    dec = dec + suma
+  }
+  return dec
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let bin = []
+  let resto = 0
+  for(let i = 0;  i < 99; i++){
 
+    resto = num % 2
+    bin.unshift(resto)
+    num = num/2
+    num = Math.trunc(num)
+    if(num < 1) {
+      break;
+    }
+  }
+  return bin.join("")
 }
 
 
