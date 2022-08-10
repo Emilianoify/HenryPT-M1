e95d7427415ee4a45230887350a8c397b833488c
unknown <galvanalvarezemiliano@hotmail.com>
2022-08-10 20:40:29 -0300

    Homework finished

--- 03-JavaScriptAvanzado-II/homework/homework.js
+++ 03-JavaScriptAvanzado-II/homework/homework.js
@@ -19,15 +19,26 @@ function counter() {
   otroContador()      // 2
   otroContador()      // 3
    */
+  let cont = 0
+  return function(){
+    cont++
+    return cont
+  }
+  
 }
 
 function cacheFunction(cb) {
   /*
   Ejercicio 2
 
-  Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback que recibe por parámetro (cb); es decir, que "recuerde" el resultado de cada operación que hace, de manera que, al realizar una operación por segunda vez, se pueda obtener el resultado de esa "memoria" sin tener que efectuar otra vez cálculos que ya se hicieron anteriormente.
+  Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback que recibe por parámetro (cb);
+   es decir, que "recuerde" el resultado de cada operación que hace, de manera que, al realizar una operación por segunda vez, 
+   se pueda obtener el resultado de esa "memoria" sin tener que efectuar otra vez cálculos que ya se hicieron anteriormente.
 
-  cacheFunction debe retornar una función. Esta función debe aceptar un argumento (arg) e invocar a cb con ese argumento; hecho eso, debe guardar el argumento junto con el resultado de la invocación (tip: usá un objeto donde cada propiedad sea el argumento, y su valor el resultado de la correspondiente invocación a cb) de manera que, la próxima vez que reciba el mismo argumento, no sea necesario volver a invocar a cb, porque el resultado estará guardado en la "memoria caché".
+  cacheFunction debe retornar una función. 
+  Esta función debe aceptar un argumento (arg) e invocar a cb con ese argumento; hecho eso, debe guardar el argumento junto con el resultado de la invocación 
+  (tip: usá un objeto donde cada propiedad sea el argumento, y su valor el resultado de la correspondiente invocación a cb) de manera que, la próxima vez que reciba el mismo argumento
+  , no sea necesario volver a invocar a cb, porque el resultado estará guardado en la "memoria caché".
 
 
   Ejemplo:
@@ -41,13 +52,25 @@ function cacheFunction(cb) {
   squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) 
 
   */
-}
+
+  let obj = {}
+  return function(arg){
+    if(obj.hasOwnProperty(arg)){
+      return obj[arg]
+    }
+    obj[arg] = cb(arg)
+    return obj[arg]
+  }
+
+  
+ }
 
 // Bind
 
 var instructor = {
   nombre: "Franco",
   edad: 25,
+  
 };
 
 var alumno = {
@@ -70,10 +93,17 @@ function getNombre() {
 let getNombreInstructor;
 let getNombreAlumno;
 
+getNombreInstructor = getNombre.bind(instructor)
+getNombreInstructor()
+
+getNombreAlumno = getNombre.bind(alumno)
+getNombreAlumno()
+
 /*
   Ejercicio 4
   
-  Sin modificar la función crearCadena, usar bind para guardar, en las tres variables declaradas a continuación, tres funciones que retornen una cadena (string) y el delimitador especificado (asteriscos, guiones, y guiones bajos, respectivamente). Las funciones obtenidas deberían recibir solamente un argumento - la cadena de texto - ya que los otros argumentos habrán sido "bindeados". 
+  Sin modificar la función crearCadena, usar bind para guardar, en las tres variables declaradas a continuación, tres funciones que retornen una cadena (string) y el delimitador especificado (asteriscos, guiones, y guiones bajos, respectivamente). 
+  Las funciones obtenidas deberían recibir solamente un argumento - la cadena de texto - ya que los otros argumentos habrán sido "bindeados". 
 */
 
 function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
@@ -83,6 +113,14 @@ function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
 let textoAsteriscos;
 let textoGuiones;
 let textoUnderscore;
+
+
+textoAsteriscos = crearCadena.bind(this, "*", "*")
+textoAsteriscos()
+textoGuiones = crearCadena.bind(this, "-", "-")
+textoGuiones()
+textoUnderscore = crearCadena.bind(this, "_", "_")
+textoUnderscore()
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
