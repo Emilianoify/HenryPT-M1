1b9555831f66c82e5fbb8399e0cce16a9252d0eb
Emilianoify <galvanalvarezemiliano@hotmail.com>
2022-08-30 19:36:28 -0300

    homework finished

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,24 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-  return array.sort(function (a, b) { return a - b })
+
+  if(array.length <= 1){
+    return array
+  }
+
+  let pivot = array[0]
+  let left = []
+  let right = []
+
+  for(let i = 1; i < array.length; i++){
+    if(array[i] < pivot){
+      left.push(array[i])
+    }else{
+      right.push(array[i])
+    }
+  }
+  return [].concat(quickSort(left), pivot, quickSort(right))
+  
 }
 
 function mergeSort(array) {
