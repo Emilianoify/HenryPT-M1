f3a38f0151329f3d4cb0c45a32ef78b14d2596bf
Emilianoify <galvanalvarezemiliano@hotmail.com>
2022-08-30 20:32:46 -0300

    homework finished

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -7,23 +7,23 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
-  if(array.length <= 1){
-    return array
+  if (array.length <= 1) {
+    return array;
   }
 
-  let pivot = array[0]
-  let left = []
-  let right = []
+  let pivot = array[0];
+  let left = [];
+  let right = [];
 
-  for(let i = 1; i < array.length; i++){
-    if(array[i] < pivot){
-      left.push(array[i])
-    }else{
-      right.push(array[i])
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < pivot) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
     }
   }
-  return [].concat(quickSort(left), pivot, quickSort(right))
-  
+  return [].concat(quickSort(left), pivot, quickSort(right));
+
 }
 
 function mergeSort(array) {
@@ -31,8 +31,44 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-  return array.sort(function (a, b) { return a - b })
+
+  if (array.length <= 1) {
+    return array;
+  }
+
+  let division = middle(array);
+  let left = division[0];
+  let right = division[1];
+  return paste(mergeSort(left), mergeSort(right));
 }
+
+function middle(array) {
+  let mitad = Math.floor(array.length / 2);
+  let izq = array.slice(0, mitad);
+  let der = array.slice(mitad);
+
+  return [izq, der];
+
+}
+
+function paste(left, right) {
+  let array = []
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      array.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      array.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return array.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
